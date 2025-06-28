0e08f46f06318b0606c401ca7b59e9762b0b9978
JAEHWAN <woghks1213@gmail.com>
2025-06-28 11:44:06 +0900

    hotfix: 자기소개서 및 면접 관련 기능 GMS -> API 수정
    - 자기소개서 작성, 채팅
    - 면접 질문 생성, 피드백

--- ai/app/api/v1/endpoints/interview.py
+++ ai/app/api/v1/endpoints/interview.py
@@ -10,45 +10,45 @@ logger = app_logger
 router = APIRouter(prefix="/interview", tags=["interview"])
 
 
-######################################################## original interview service ########################################################
-
-# @router.post("/question/cover-letter")
-# async def create_interview_question_from_cover_letter(request: interview.CreateQuestionRequest)->interview.CreateQuestionResponse:
-
-#     # 면접 질문 생성 서비스 호출
-#     questions = await interview_service.create_interview_questions_from_cover_letter(request)
-    
-#     return interview.CreateQuestionResponse(
-#         cover_letter_id=request.cover_letter.cover_letter_id,
-#         expected_questions=questions
-#     )
-    
-
-# @router.post("/feedback")
-# async def feedback_interview(request: interview.FeedbackInterviewRequest)->interview.FeedbackInterviewResponse:
-    
-#     result = await interview_service.feedback_interview(request)
-    
-#     return result
-
-
-######################################################## gms interview service ########################################################
+####################################################### original interview service ########################################################
 
 @router.post("/question/cover-letter")
-async def create_interview_question_from_cover_letter_gms(request: interview.CreateQuestionRequest)->interview.CreateQuestionResponse:
+async def create_interview_question_from_cover_letter(request: interview.CreateQuestionRequest)->interview.CreateQuestionResponse:
 
-    questions = await gms_interview_service.create_interview_questions_from_cover_letter(request)
+    # 면접 질문 생성 서비스 호출
+    questions = await interview_service.create_interview_questions_from_cover_letter(request)
     
-    response = interview.CreateQuestionResponse(
+    return interview.CreateQuestionResponse(
         cover_letter_id=request.cover_letter.cover_letter_id,
         expected_questions=questions
     )
-    logger.info(f"CreateInterviewQuestionFromCoverLetterGMSResponse: {response}")
-    return response
+    
 
 @router.post("/feedback")
-async def feedback_interview_gms(request: interview.FeedbackInterviewRequest)->interview.FeedbackInterviewResponse:
+async def feedback_interview(request: interview.FeedbackInterviewRequest)->interview.FeedbackInterviewResponse:
     
-    result = await gms_interview_service.feedback_interview(request)
-    logger.info(f"FeedbackInterviewGMSResponse: {result}")
+    result = await interview_service.feedback_interview(request)
+    
     return result
+
+
+# ######################################################## gms interview service ########################################################
+
+# @router.post("/question/cover-letter")
+# async def create_interview_question_from_cover_letter_gms(request: interview.CreateQuestionRequest)->interview.CreateQuestionResponse:
+
+#     questions = await gms_interview_service.create_interview_questions_from_cover_letter(request)
+    
+#     response = interview.CreateQuestionResponse(
+#         cover_letter_id=request.cover_letter.cover_letter_id,
+#         expected_questions=questions
+#     )
+#     logger.info(f"CreateInterviewQuestionFromCoverLetterGMSResponse: {response}")
+#     return response
+
+# @router.post("/feedback")
+# async def feedback_interview_gms(request: interview.FeedbackInterviewRequest)->interview.FeedbackInterviewResponse:
+    
+#     result = await gms_interview_service.feedback_interview(request)
+#     logger.info(f"FeedbackInterviewGMSResponse: {result}")
+#     return result
